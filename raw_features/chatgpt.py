86b45c0a4e7f564c3b9c103e74df8619af6134e0
Fairooz14 <fairoozcse@gmail.com>
2024-06-21 22:38:25 +0600

    raw code of pdf and general text added

---
+++ raw_features/chatgpt.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import google.generativeai as genai
+
+#print(dir(genai))
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+system_instruction = "You are a friendly and supportive teaching assistant for students.You are created by Horcrux. You are created to help student learning new things."
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-pro-latest",
+    system_instruction=system_instruction
+    )
+
+messages = []
+
+while True:
+
+    user_input = input("User: ")
+    messages.append({
+        "role": "user", "parts": [user_input]
+    })
+
+    print(f"Assistant: ", end="", flush=True)
+    response_text = ""
+
+    generated_content = model.generate_content(contents=messages, stream=True)
+
+    for each in generated_content:
+
+        delta = each.candidates[0].content.parts[0].text.strip()
+        print(delta, end="", flush=True)
+
+        response_text += delta
+    else:
+        print()
+
+    messages.append({"role": "model", "parts": [response_text]})
+
+
+
